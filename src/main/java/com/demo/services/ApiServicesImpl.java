67984026d144808d52f9e2a0c70d45d0d6d92964
desiCoda <j.pushkarsingh@gmail.com>
2018-04-29 00:37:12 +0530

    updated restTempalteConfig class: added RestTemplate bean and marked class as a configuration component

--- src/main/java/com/demo/services/ApiServicesImpl.java
+++ src/main/java/com/demo/services/ApiServicesImpl.java
@@ -2,8 +2,10 @@ package com.demo.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.domain.User;
 import com.domain.UserData;
@@ -12,15 +14,19 @@ import com.domain.UserData;
 public class ApiServicesImpl implements ApiServices {
 
 	private RestTemplate restTemplate;
+	private String url;
 
-	public ApiServicesImpl(RestTemplate restTemplate) {
+	public ApiServicesImpl(RestTemplate restTemplate, @Value("${user.api.url}") String url) {
 		this.restTemplate = restTemplate;
+		this.url = url;
 	}
 
 	@Override
 	public List<User> getUsers(Integer limit) {
-		UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit"+limit,UserData.class);
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(url).queryParam("limit", limit);
 		
+		UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
+
 		return userData.getData();
 	}
 
