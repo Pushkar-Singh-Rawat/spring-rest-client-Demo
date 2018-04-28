6bb77c5410d8098f8b718d985030d60cf3e697b3
desiCoda <j.pushkarsingh@gmail.com>
2018-04-29 00:34:46 +0530

    added integration test: ApiServiceImplTest.java

---
+++ src/test/java/com/demo/service/ApiServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.demo.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.demo.services.ApiServices;
+import com.domain.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiServiceImplTest {
+
+	@Autowired
+	ApiServices apiServices;
+
+	@Before
+	public void setup() {
+
+	}
+
+	@Test
+	public void testGetUsers() throws Exception {
+		List<User> listOfUsers = apiServices.getUsers(3);
+		assertEquals(4, listOfUsers.size());
+	}
+}
